76e78143cd7196c3f5d5ba9c6bb6291ea8b18b89
Jakub Jaluvka <jakub.jaluvka@bluelemons.sk>
2022-10-19 14:44:07 +0200

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import "./App.css"
 
 type Value = "x" | "o" | null
 type Board = Value[][] | null
+type GameState =
+  | "playerTurnX"
+  | "playerTurnO"
+  | "playerWinX"
+  | "playerWinO"
+  | "draw"
 
 function App() {
   const [boardState, setBoardState] = useState<Board>(null)
-  const [playerSwitch, setPlayerSwitch] = useState<boolean>(false)
-  const [win, setWin] = useState<boolean>(false)
-  const [draw, setDraw] = useState<boolean>(false)
   const [endResult, setEndResult] = useState<string>("")
+  const [gameState, setGameState] = useState<GameState>("playerTurnX")
+
+  console.log(gameState)
 
   const boardDimensions = (size: number) => {
     setBoardState(
@@ -21,30 +27,42 @@ function App() {
   }
 
   const checkRows = (i: number) => {
-    if (!boardState![i].includes("x") && !boardState![i].includes(null))
-      setWin(true)
-    if (!boardState![i].includes("o") && !boardState![i].includes(null))
-      setWin(true)
+    if (!boardState![i].includes("x") && !boardState![i].includes(null)) {
+      setGameState("playerWinO")
+    }
+    if (!boardState![i].includes("o") && !boardState![i].includes(null)) {
+      setGameState("playerWinX")
+    }
   }
 
   const checkCollumns = (i: number) => {
     if (
       !boardState!.map((row) => row[i]).includes("x") &&
       !boardState!.map((row) => row[i]).includes(null)
-    )
-      setWin(true)
+    ) {
+      setGameState("playerWinO")
+    }
     if (
       !boardState!.map((row) => row[i]).includes("o") &&
       !boardState!.map((row) => row[i]).includes(null)
-    )
-      setWin(true)
+    ) {
+      setGameState("playerWinX")
+    }
   }
 
   const checkDiagonals = (diagonalA: Value[], diagonalB: Value[]) => {
-    if (!diagonalA.includes("x") && !diagonalA.includes(null)) setWin(true)
-    if (!diagonalA.includes("o") && !diagonalA.includes(null)) setWin(true)
-    if (!diagonalB.includes("x") && !diagonalB.includes(null)) setWin(true)
-    if (!diagonalB.includes("o") && !diagonalB.includes(null)) setWin(true)
+    if (!diagonalA.includes("x") && !diagonalA.includes(null)) {
+      setGameState("playerWinO")
+    }
+    if (!diagonalA.includes("o") && !diagonalA.includes(null)) {
+      setGameState("playerWinX")
+    }
+    if (!diagonalB.includes("x") && !diagonalB.includes(null)) {
+      setGameState("playerWinO")
+    }
+    if (!diagonalB.includes("o") && !diagonalB.includes(null)) {
+      setGameState("playerWinX")
+    }
   }
 
   const winCondition = () => {
@@ -65,39 +83,36 @@ function App() {
   const drawCondition = () => {
     const drawCheck = boardState!.map((row) => row.includes(null))
     if (!drawCheck.includes(true)) {
-      setDraw(true)
+      setGameState("draw")
     }
   }
 
   useEffect(() => {
-    if (win && playerSwitch) setEndResult("Player 1 (x) has won!")
-    if (win && !playerSwitch) setEndResult("Player 2 (o) has won!")
-    if (!win && draw) setEndResult("It's a draw!!")
-  }, [win, draw])
+    if (gameState === "playerWinX") setEndResult("Player 1 (x) has won!")
+    if (gameState === "playerWinO") setEndResult("Player 2 (o) has won!")
+    if (gameState === "draw") setEndResult("It's a draw!!")
+  }, [gameState])
 
   const onClick = (cellValue: Value, rowIndex: number, cellIndex: number) => {
     const boardCopy = [...boardState!]
-    if (!cellValue && !playerSwitch && !win) {
+    if (!cellValue && gameState === "playerTurnX") {
       boardCopy[rowIndex][cellIndex] = "x"
     }
-    if (!cellValue && playerSwitch && !win) {
+    if (!cellValue && gameState === "playerTurnO") {
       boardCopy[rowIndex][cellIndex] = "o"
     }
     setBoardState(boardCopy)
-    setPlayerSwitch(!playerSwitch)
-    winCondition()
+    if (gameState === "playerTurnO") setGameState("playerTurnX")
+    if (gameState === "playerTurnX") setGameState("playerTurnO")
     drawCondition()
+    winCondition()
   }
 
   return (
     <div className="App">
       <div className="gameInfo">
-        {!playerSwitch && !win && !draw && boardState && (
-          <p>Player one's turn</p>
-        )}
-        {playerSwitch && !win && !draw && boardState && (
-          <p>Player two's turn</p>
-        )}
+        {gameState === "playerTurnX" && boardState && <p>Player one's turn</p>}
+        {gameState === "playerTurnO" && boardState && <p>Player two's turn</p>}
         {endResult && <p>{endResult}</p>}
       </div>
       {!boardState && (
@@ -133,15 +148,15 @@ function App() {
           </tbody>
         </table>
       )}
-      {(win || draw) && (
+      {(gameState === "playerWinX" ||
+        gameState === "playerWinO" ||
+        gameState === "draw") && (
         <button
           className="buttons"
           onClick={() => {
             setBoardState(null)
-            setWin(false)
-            setDraw(false)
+            setGameState("playerTurnX")
             setEndResult("")
-            setPlayerSwitch(false)
           }}
         >
           Reset board?
